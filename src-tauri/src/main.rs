887dc8ddf7b4541b5c13d7f4b5cd6543c2d5c40f
jerr-it <jrgls.no-reply@gmx.de>
2023-02-05 17:21:38 +0100

    Added tauri logout command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -38,6 +38,18 @@ fn nordvpn_is_logged_in() -> Result<bool, String> {
         .contains("You are not logged in."))
 }
 
+#[tauri::command]
+fn nordvpn_logout() -> Result<bool, String> {
+    let output = Command::new("nordvpn")
+        .arg("logout")
+        .output()
+        .map_err(|e| e.to_string())?;
+
+    Ok(String::from_utf8_lossy(&output.stdout)
+        .to_string()
+        .contains("You are logged out."))
+}
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
