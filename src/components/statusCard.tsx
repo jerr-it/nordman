20c33b4e444942cceba9dcc6564ca20034d016f2
jerr-it <jrgls.no-reply@gmx.de>
2023-02-08 18:04:58 +0100

    Fixed button in button error

--- src/components/statusCard.tsx
+++ src/components/statusCard.tsx
@@ -1,4 +1,4 @@
-import { ButtonBase, Card, Collapse, Divider, IconButton, ListItemText, Snackbar, Stack, Typography } from "@mui/material";
+import { Box, ButtonBase, Card, Collapse, Divider, IconButton, ListItemText, Snackbar, Stack, Typography } from "@mui/material";
 import { country_converter } from "../assets/country_converter";
 import CancelIcon from '@mui/icons-material/Cancel';
 import { useState } from "react";
@@ -13,38 +13,41 @@ function StatusCard({ connection }: { connection: ConnectionDetails | null }) {
             open={connection !== null}
             anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
         >
-            <ButtonBase onClick={() => setCollapsed(!collapsed)}>
-                <Card sx={{ minWidth: 200, minHeight: 70, maxWidth: 445, p: 1.25, backgroundColor: "green" }}>
-                    <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
-                        <span className={"fi fi-" + country_converter(connection?.country ?? "-")} style={{ width: 50, height: 50 }}></span>
-                        <ListItemText primary={connection?.city ?? "-"} secondary={connection?.country ?? "-"} />
-                        <ListItemText primary={connection?.ip ?? "-"} secondary={connection?.hostname ?? "-"} />
-                        <IconButton
-                            color="inherit"
-                            onClick={(e) => { e.stopPropagation() }}
-                            onMouseDown={(e) => { e.stopPropagation() }}
-                        >
-                            <CancelIcon />
-                        </IconButton>
-                    </Stack>
-                    <Collapse
-                        in={collapsed}
-                    >
-                        <Divider sx={{ m: 1 }} />
-                        <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
-                            <Stack direction="column">
-                                <Typography variant="body2">{connection?.transfer.split(",")[0] ?? "-"}</Typography>
-                                <Typography variant="body2">{connection?.transfer.split(",")[1] ?? "-"}</Typography>
-                            </Stack>
-                            <Stack direction="column">
-                                <Typography variant="body2">{connection?.current_technology}</Typography>
-                                <Typography variant="body2">{connection?.current_protocol}</Typography>
-                            </Stack>
-                            <Typography variant="body2">{connection?.uptime}</Typography>
+            <Box>
+                <ButtonBase onClick={() => setCollapsed(!collapsed)}>
+                    <Card sx={{ minWidth: 200, minHeight: 70, maxWidth: 445, p: 1.25, backgroundColor: "green" }}>
+                        <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2} sx={{ mr: 5 }}>
+                            <span className={"fi fi-" + country_converter(connection?.country ?? "-")} style={{ width: 50, height: 50 }}></span>
+                            <ListItemText primary={connection?.city ?? "-"} secondary={connection?.country ?? "-"} />
+                            <ListItemText primary={connection?.ip ?? "-"} secondary={connection?.hostname ?? "-"} />
                         </Stack>
-                    </Collapse>
-                </Card>
-            </ButtonBase>
+                        <Collapse
+                            in={collapsed}
+                        >
+                            <Divider sx={{ m: 1 }} />
+                            <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
+                                <Stack direction="column">
+                                    <Typography variant="body2">{connection?.transfer.split(",")[0] ?? "-"}</Typography>
+                                    <Typography variant="body2">{connection?.transfer.split(",")[1] ?? "-"}</Typography>
+                                </Stack>
+                                <Stack direction="column">
+                                    <Typography variant="body2">{connection?.current_technology}</Typography>
+                                    <Typography variant="body2">{connection?.current_protocol}</Typography>
+                                </Stack>
+                                <Typography variant="body2">{connection?.uptime}</Typography>
+                            </Stack>
+                        </Collapse>
+                    </Card>
+                </ButtonBase>
+                <IconButton
+                    color="inherit"
+                    sx={{ position: "absolute", top: 0, right: 0 }}
+                    onClick={(e) => { e.stopPropagation() }}
+                    onMouseDown={(e) => { e.stopPropagation() }}
+                >
+                    <CancelIcon />
+                </IconButton>
+            </Box>
         </Snackbar>
     );
 }
