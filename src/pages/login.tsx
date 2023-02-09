b93ab38a9e0785ceb78237f1211e588398ffe2d7
jerr-it <jrgls.no-reply@gmx.de>
2023-02-09 20:41:37 +0100

    Reworked login, can now open the login url again by pressing the button, removed maximum number of tries in favor of regulary checking after first press

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -9,22 +9,25 @@ import { useNavigate } from 'react-router-dom';
 import ThemeSwitchButton from "../components/themeSwitchButton";
 
 function LoginPage() {
-    const [waiting, setWaiting] = useState(true);
+    const [state, setState] = useState<{ waiting: boolean, login_url: string }>({ waiting: false, login_url: "" });
 
     const navigate = useNavigate();
 
     async function StartLogin() {
         try {
-            const url: string = await invoke("nordvpn_login", {});
-            await open(url);
-            setWaiting(true);
+            if (!state.waiting) {
+                const url: string = await invoke("nordvpn_login", {});
+                await open(url);
+                setState({ waiting: true, login_url: url });
+            } else {
+                await open(state.login_url);
+            }
         } catch (e) {
             console.error(e);
         }
     }
 
     async function check_login(): Promise<boolean> {
-        console.log("check");
         try {
             return await invoke("nordvpn_is_logged_in", {});
         } catch (e) {
@@ -37,38 +40,28 @@ function LoginPage() {
     // Initial check
     useEffect(() => {
         check_login().then((status) => {
-            if(status) {
+            if (status) {
                 navigate("/dashboard");
-            } else {
-                setWaiting(false);
             }
         });
     }, []);
 
     // Check every 3 seconds after pressing login
-    let failCounter = 0;
     useEffect(() => {
         const repeat = setInterval(async () => {
-            if(!waiting) return;
+            if (!state.waiting) return;
 
             const status = await check_login();
             if (status) {
-                console.log("Logged in!");
                 clearInterval(repeat);
                 navigate("/dashboard");
-            } else {
-                failCounter++;
-                if (failCounter >= 5) {
-                    setWaiting(false);
-                    clearInterval(repeat);
-                }
             }
         }, 3000);
 
         return () => {
             clearInterval(repeat);
         }
-    }, [waiting]);
+    }, [state.waiting]);
 
     return (
         <Grid
@@ -79,27 +72,26 @@ function LoginPage() {
             justifyContent="center"
             style={{ minHeight: '100vh' }}
         >
-            <Card sx={{m:1, p:5, minWidth:400}}>
+            <Card sx={{ m: 1, p: 5, minWidth: 400 }}>
                 <Stack direction="column" alignItems="center">
                     <Typography variant="h3" align="center">Login</Typography>
-                    <Button 
-                        sx={{mt:3, minWidth:200}}
-                        variant="contained" 
-                        startIcon={<LoginIcon/>}
+                    <Button
+                        sx={{ mt: 3, minWidth: 200 }}
+                        variant="contained"
+                        startIcon={<LoginIcon />}
                         onClick={StartLogin}
-                        disabled={waiting}
                     >
                         Browser Login
                     </Button>
-                    {waiting && 
-                        <Stack sx={{m:3}} direction="row" alignItems="center">
-                            <CircularProgress/>
-                            <Typography sx={{m:1}} align="center">Waiting for login...</Typography>
+                    {state.waiting &&
+                        <Stack sx={{ m: 3 }} direction="row" alignItems="center">
+                            <CircularProgress />
+                            <Typography sx={{ m: 1 }} align="center">Waiting for login...</Typography>
                         </Stack>
                     }
                 </Stack>
             </Card>
-            <ThemeSwitchButton/>
+            <ThemeSwitchButton />
         </Grid>
     );
 }
